fbbc3fd679834203b71d901de32c01818598650d
Tomas Chudjak <tomas.chudajk@gmail.com>
2020-10-12 16:59:53 +0200

    added: custom opvn config generator

--- make-config.sh
+++ make-config.sh
@@ -10,17 +10,60 @@ fi
 KEY_DIR=~/openvpn-ca/keys
 OUTPUT_DIR=~/client-configs/files
 BASE_CONFIG=~/client-configs/base.conf
+USER_DIR=/home/ubuntu/openvpn/${name}
 
-cat ${BASE_CONFIG} \
-    <(echo -e '<ca>') \
-    ${KEY_DIR}/ca.crt \
-    <(echo -e '</ca>\n<cert>') \
-    ${KEY_DIR}/${name}.crt \
-    <(echo -e '</cert>\n<key>') \
-    ${KEY_DIR}/${name}.key \
-    <(echo -e '</key>\n<tls-auth>') \
-    ${KEY_DIR}/ta.key \
-    <(echo -e '</tls-auth>') \
-    > ${OUTPUT_DIR}/${name}.ovpn
+rm -rf ${USER_DIR}
+rm -rf ${USER_DIR}.zip
 
-# sed -i "s/group nogroup/group nobody/" ${OUTPUT_DIR}/${name}.ovpn
+mkdir -p ${USER_DIR}
+
+for ITEM in ${KEY_DIR}/${name}.crt ${KEY_DIR}/ca.crt ${KEY_DIR}/${name}.key ${KEY_DIR}/ta.key
+do
+  cp ${ITEM} ${USER_DIR}
+done
+
+# cat ${BASE_CONFIG} \
+#   <(echo -e '<ca>') \
+#   ${KEY_DIR}/ca.crt \
+#   <(echo -e '</ca>\n<cert>') \
+#   ${KEY_DIR}/${name}.crt \
+#   <(echo -e '</cert>\n<key>') \
+#   ${KEY_DIR}/${name}.key \
+#   <(echo -e '</key>\n<tls-auth>') \
+#   ${KEY_DIR}/ta.key \
+#   <(echo -e '</tls-auth>') \
+#   > ${OUTPUT_DIR}/${name}.ovpn
+
+TA_VALUE=`cat ${KEY_DIR}/ta.key`
+CA_VALUE=`cat ${KEY_DIR}/ca.crt`
+CERT_VALUE=`cat ${KEY_DIR}/${name}.crt`
+KEY_VALUE=`cat ${KEY_DIR}/${name}.key`
+
+echo "
+<tls-auth>
+${TA_VALUE}
+</tls-auth>" >> ${OUTPUT_DIR}/${name}.ovpn
+
+echo "
+<ca>
+${CA_VALUE}
+</ca>" >> ${OUTPUT_DIR}/${name}.ovpn
+
+echo "
+<cert>
+${CERT_VALUE}
+</cert>" >> ${OUTPUT_DIR}/${name}.ovpn
+
+echo "
+<key>
+${KEY_VALUE}
+</key>" >> ${OUTPUT_DIR}/${name}.ovpn
+
+cp ${OUTPUT_DIR}/${name}.ovpn ${USER_DIR}
+
+cd ${USER_DIR}/../
+
+zip -r ${name}.zip ${name}
+
+chown ubuntu:ubuntu ${USER_DIR}
+chown ubuntu:ubuntu ${USER_DIR}.zip
